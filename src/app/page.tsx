a3098c511a62bf5c6d322a439f40c3fa5dce5fc7
marjanmokhtari <mimmokhtarii@gmail.com>
2024-02-29 19:15:16 +0330

    add removetask and local storage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,35 @@
 "use client"
 import { log } from 'console'
 import React, { createRef, useContext, useEffect, useRef, useState } from 'react'
-import { createContext } from 'vm'
+import { IoClose } from "react-icons/io5";
+
 
 
 export default function page() {
-  const [task, setTask] = useState([])
-  const [mydate, setMydate] = useState([])
-  const [times,setTimes]=useState([])
+  const [task, setTask] = useState(() => JSON.parse(localStorage.getItem('task')) || [])
+  const [mydate, setMydate] = useState(() => JSON.parse(localStorage.getItem('dates')) || [])
+  const [times, setTimes] = useState(() => JSON.parse(localStorage.getItem('times')) || [])
 
-  function handel(newtask, newDate,newTime) {
-    setTask([...task, newtask])
-    setMydate([...mydate, newDate])
-    setTimes([...times,newTime])
+  function handel(newtask, newDate, newTime) {
 
+    const updatetasks = [...task, newtask]
+    const updateDate = [...mydate, newDate]
+    const updateTimes = [...times, newTime]
+    setTask(updatetasks)
+    setTimes(updateTimes)
+    setMydate(updateDate)
 
+    localStorage.setItem('task',JSON.stringify(updatetasks))
+    localStorage.setItem('dates',JSON.stringify(updateDate))
+    localStorage.setItem('times',JSON.stringify(updateTimes))
   }
+
+  function remove_task(index){
+    const updatedTasks = task.filter((_, i) => i !== index);
+    setTask(updatedTasks)
+    localStorage.setItem('task',JSON.stringify(updatedTasks));
+  }
+ 
 
   return (
     <main className='w-full h-[100vh] flex justify-center items-center'>
@@ -24,7 +38,7 @@ export default function page() {
           <ToDo tackchnge={handel} ></ToDo>
         </section>
         <section className='w-[40%] ms-2'>
-          <List task={task} dates={mydate} times={times}></List>
+          <List task={task} dates={mydate} times={times} remove_task={remove_task}></List>
         </section>
       </section>
     </main>
@@ -35,16 +49,14 @@ export default function page() {
 function ToDo({ tackchnge }) {
   const [myval, setMyval] = useState('')
   const [mydate, setMydate] = useState('')
-  const [mytime,setMytime]=useState('')
+  const [mytime, setMytime] = useState('')
 
   function Addpost() {
     const newTask = myval
     const newDate = mydate
-    const newTime=mytime
-    tackchnge(newTask, newDate,newTime);
-    setMydate('')
+    const newTime = mytime
+    tackchnge(newTask, newDate, newTime);
     setMyval('')
-    setMytime('')
 
 
   }
@@ -60,7 +72,7 @@ function ToDo({ tackchnge }) {
       </section>
       <section className='flex *:w-1/2 *:border *:border-gray-600 *:outline-none *:py-2 *:px-2 py-10'>
         <input value={mydate} onChange={(e) => setMydate(e.target.value)} className='me-2' type="date" />
-        <input value={mytime} onChange={(e)=>setMytime(e.target.value)} className='ms-2' type="time" />
+        <input value={mytime} onChange={(e) => setMytime(e.target.value)} className='ms-2' type="time" />
       </section>
       <section>
         <textarea value={myval} onChange={(e) => setMyval(e.target.value)} placeholder='what do you need done today?' name="" id="" className=' w-full border border-gray-600 resize-none  p-3 outline-none focus:outline-2 focus:outline-offset-0 focus:outline-gray-600'></textarea>
@@ -73,21 +85,29 @@ function ToDo({ tackchnge }) {
   )
 }
 
-function List({ task, dates,times }) {
+function List({ task, dates, times,remove_task }) {
+
+
 
   return (
-    <section className='w-full p-5'>
+    <section className='w-full p-5 cursor-pointer'>
       <section className='flex justify-center'>
-        <h2 className='text-3xl font-extrabold text-[#474747]'>List</h2>
+        <h2 className='text-3xl font-extrabold text-[#474747] '>List</h2>
       </section>
-      <section className='flex *:w-1/2 justify-between '>
-        <section>
+      <section className='flex w-full py-10  '>
+        <section className=' w-full *:my-2'>
           {task.map((task, index) => (
             <>
-              <section key={index}>
-                <h2>{task}</h2>
-                <span>{dates[index]}</span>
-                <span>{times[index]}</span>
+              <section key={index} className=' border border-gray-600 w-full rounded-md p-3'>
+                <div className=' flex justify-between '>
+                  <h2 className=' text-xl font-semibold'>{task}</h2>
+                  <IoClose onClick={()=>remove_task(index)} className=' flex items-center text-xl '></IoClose>
+
+                </div>
+                <div className=' flex  '>
+                  <span className='me-2'>{dates[index]}</span>
+                  <span className='ms-2'>{times[index]}</span>
+                </div>
 
               </section>
             </>
